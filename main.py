b458911234962a3ebd28a3c9f3a1ed4197926cb2
Anton Karazeev <anton.karazeev@gmail.com>
2023-06-12 22:46:02 +0100

    Log rpi cpu temperature

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-import influxdb_client
 import os, time, random
+import influxdb_client
 from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+try:
+    from gpiozero import CPUTemperature
+    cpu = CPUTemperature()
+    is_rpi = True
+except:
+    is_rpi = False
+
 # token = os.environ.get("INFLUXDB_TOKEN")
-# token = "WkoTClqWxUZ_zCoUn3RZsMNVehVG8MeYFy_R9yaBUEbwmxgAxvJVdIkMdHJhVmXqoJ6jJxWAJXoA3sPWgMPxKQ=="
-token = "4hU5PecOk-P1exVuYssI9IOcsnUfT-j6K7aQ7rlmj4MnDRP-g6Qwydo7jlqHlCZfvCk8Fk3s5vTAj0_s8NzHGw=="
+token = "BnqYzKHq2CrCr98b9EknZUTMkytqaK_5SmRRft-ulKBdLezPRANHkBUgKiUGi7DShGJxs9vu4r9xEaWy590Kdw=="
+
 org = "temp-monitor-org"
 url = "http://localhost:8086"
-
 influx_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
 
 bucket = "temp-monitor-bucket"
 write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
-for _ in range(5):
-    val = random.random()
-    val = float(f"{val:.2}")
+# for _ in range(5):
+try:
+    while True:
+        if is_rpi:
+            val = cpu.temperature
+        else:
+            val = random.random()
+            val = float(f"{val:.2}")
 
-    point = (
-        Point("measurement")
-        .tag("source", "random")
-        .field("temperature", val)
-    )
-    write_api.write(bucket=bucket, org="temp-monitor-org", record=point)
-    print(point)
-    time.sleep(1)  # separate points by 1 second
-
+        point = (
+            Point("measurement")
+            .tag("source", "random")
+            .field("temperature", val)
+        )
+        write_api.write(bucket=bucket, org=org, record=point)
+        print(point)
+        time.sleep(1)  # separate points by 1 second
+except:
+    exit(1)
